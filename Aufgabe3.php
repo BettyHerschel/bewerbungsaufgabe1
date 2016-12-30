09d06b76d86fec1f730a91a9640f90a533456406
BettyHerschel <lisaoeleker@hotmail.de>
2016-12-30 15:55:35 +0100

    Erste Version Aufgabe 3

---
+++ Aufgabe3.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Aufgabe3</title>
+</head>
+
+<body>
+
+<form action="action_page.php" method="post">
+    Datum:<br>
+    <input type="datetime-local" name="termindatum" placeholder="1999-12-24T06:39:57" required><br>
+    Nachricht:<br>
+    <textarea  minlength="1" maxlength="255" rows="4" cols="50" required></textarea>
+    <br><br>
+    <input type="submit" value="Speichern">
+</form>
+
+
+
+</body>
+
+</html>
+
+
