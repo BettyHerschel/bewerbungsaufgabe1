de0700edfe39299dd0abfcf953eac7e9d8432703
BettyHerschel <lisaoeleker@hotmail.de>
2016-12-30 16:39:27 +0100

    Erste Version mit PHP Aufgabe 3

--- Aufgabe3.php
+++ Aufgabe3.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,16 +7,19 @@
 
 <body>
 
-<form action="action_page.php" method="post">
+<form action="Aufgabe3.php" method="get">
     Datum:<br>
     <input type="datetime-local" name="termindatum" placeholder="1999-12-24T06:39:57" required><br>
     Nachricht:<br>
-    <textarea  minlength="1" maxlength="255" rows="4" cols="50" required></textarea>
+    <textarea name="nachricht" minlength="1" maxlength="255" rows="4" cols="50" required></textarea>
     <br><br>
     <input type="submit" value="Speichern">
 </form>
 
-
+<?php
+echo $_GET["termindatum"];
+echo $_GET["nachricht"];
+?>
 
 </body>
 
