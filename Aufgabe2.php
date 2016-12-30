a479d1d6af54a72729e6f53602d50dba1e83521c
BettyHerschel <lisaoeleker@hotmail.de>
2016-12-30 15:14:28 +0100

    For-Schleife hinzugefügt

--- Aufgabe2.php
+++ Aufgabe2.php
@@ -1,31 +1,26 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Lisa
- * Date: 29.12.2016
- * Time: 16:24
- */
-
 function zahlenEinsBisZweihundert()
 {
-
-    return "mein erster test";
+    for ($i = 1; $i <= 200; $i++) {
+        $tauscheZahlZuWort = tauscheZahlZuWort($i);
+        echo $tauscheZahlZuWort . "\n";
+    }
 }
 
 function tauscheZahlZuWort($eingabe)
 {
     $bearbeiteterWert = $eingabe;
     if (($eingabe % 2) == 0) {
-      $bearbeiteterWert = "Zwei";
+        $bearbeiteterWert = "Zwei";
     }
     if (($eingabe % 7) == 0) {
         $bearbeiteterWert = "Sieben";
     }
-    if (($eingabe % 2) == 0 && ($eingabe % 7) == 0 ){
+    if (($eingabe % 2) == 0 && ($eingabe % 7) == 0) {
         $bearbeiteterWert = "ZweiSieben";
     }
     return $bearbeiteterWert;
 }
 
-echo tauscheZahlZuWort(7);
+zahlenEinsBisZweihundert();
